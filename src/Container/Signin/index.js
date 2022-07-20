5d8023eca1e6b3ddefcfe7ba0821b3a53b522531
shivani <shivanichourasiya18@gmail.com>
2022-07-20 19:04:46 +0530

    20july e-com application

--- src/Container/Signin/index.js
+++ src/Container/Signin/index.js
@@ -1,31 +1,52 @@
-import React from 'react'
+import React, { useState} from 'react'
 import Layout from '../../Component/Layout';
 import { Button, Form, Container, Row, Col } from 'react-bootstrap';
 import Input from '../../Component/UI/Input';
+import { login } from '../../actions';
+import { useDispatch } from 'react-redux';
 
+const Signin =(props) =>{
 
-function Signin(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const dispatch = useDispatch();
+    const userLogin = (e) => {
+
+        e.preventDefault();
+        const user = {
+            email,password
+            // email: "preeti@gmail.com",
+            // password: '12345678'
+        }
+       dispatch(login(user));
+    }
+
     return (
         <div>
+
+
             <Layout>
                 <Container>
                     <Row style={{ marginTop: '50px' }}>
                         <Col md={{ span: 6, offset: 3 }}>
-                            <Form>
+                            {/* <Form onSubmit={() => login({ email:'preeti@gmail.com', password : '12345678'})}> */}
+                            <Form onSubmit={ userLogin }>
                                 <Input
                                     label="Email"
                                     placeholder="Enter Email"
-                                    value=""
+                                    value= {email}
                                     type="email"
-                                    onChange={() => { }}
+                                    onChange={(e) => {setEmail(e.target.value) }}
 
                                 />
                                 <Input
                                     label="Password"
                                     placeholder="Enter Password"
-                                    value=""
+                                    value= {password}
                                     type="password"
-                                    onChange={() => { }}
+                                    onChange={(e) => {setPassword(e.target.value) }}
                                 />
                                 <Button variant="primary" type="submit">
                                     Submit
